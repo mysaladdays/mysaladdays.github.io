6d26dd86615d09189a70aa366f988561233ed696
mysaladdays <xujessjiang@gmail.com>
2017-01-14 13:13:56 -0500

    Update app.R

--- app.R
+++ app.R
@@ -75,9 +75,9 @@ ui <- shinyUI(fluidPage(
       tabPanel("Risk Contagion Network Monthly", forceNetworkOutput("force1")),
       tabPanel("Risk Contagion Network Monthly Large Jumps at 3-Sigma Level", forceNetworkOutput("force2")),
       tabPanel("Risk Contagion Network Monthly Large Jumps at 5-Sigma Level", forceNetworkOutput("force3")),
-      tabPanel("Risk Contagion Network Weekly", forceNetworkOutput("force4")),
-      tabPanel("Risk Contagion Network Weekly Large Jumps at 3-Sigma Level", forceNetworkOutput("force5")),
-      tabPanel("Risk Contagion Network Weekly Large Jumps at 5-Sigma Level", forceNetworkOutput("force6"))
+ #     tabPanel("Risk Contagion Network Weekly", forceNetworkOutput("force4")),
+  #    tabPanel("Risk Contagion Network Weekly Large Jumps at 3-Sigma Level", forceNetworkOutput("force5")),
+   #   tabPanel("Risk Contagion Network Weekly Large Jumps at 5-Sigma Level", forceNetworkOutput("force6"))
     )
   )
 ))
